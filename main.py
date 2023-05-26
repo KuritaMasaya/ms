b5c6c6022e6329aaab1520e5bfefbcd1ad492597
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-05-26 22:53:22 +0900

    🔨 main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title='ms'
+
+)
 
 @app.get("/")
 def read_root():
@@ -9,3 +13,10 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+def start():
+    uvicorn.run("main:app", host="0.0.0.0", port=443, reload=False)
+
+
+if __name__ == "__main__":
+    start()
